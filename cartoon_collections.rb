3314b59ae36acb41c0107230bc6d0e2c5529be1a
Neeraj Khosla <n.khosla91@gmail.com>
2019-08-20 21:23:14 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -20,6 +20,6 @@ end
 
 def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
-  array.find {|cheese| cheese_types.include(cheese)}
+  array.find {|cheese| cheese_types.include?(cheese)}
   
 end
